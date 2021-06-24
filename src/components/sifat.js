8721d59745c0e54addcc5dad74dc76feba6be357
shemuls <shemulpro@gmail.com>
2021-06-25 00:59:09 +0600

    Install React

---
+++ src/components/sifat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class sifat extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
